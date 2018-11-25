6bc048ee66bb787f77462ad96701c3fa9d70feb4
Alek Merani <alekmerani@gmail.com>
2018-11-24 21:34:29 -0600

    Add vscode

---
+++ vscode/install-generic-extensions.sh
@@ -0,0 +1,28 @@
+
+# environment variable
+code --install-extension mikestead.dotenv
+
+# formatting
+code --install-extension esbenp.prettier-vscode
+code --install-extension HookyQR.beautify
+code --install-extension SirTori.indenticator
+code --install-extension Tyriar.sort-lines
+
+# html
+code --install-extension abusaidm.html-snippets
+code --install-extension ecmel.vscode-html-css
+code --install-extension peakchen90.open-html-in-browser
+code --install-extension sidthesloth.html5-boilerplate
+
+# git
+code --install-extension donjayamanne.githistory
+code --install-extension eamodio.gitlens
+
+# markdown
+code --install-extension hnw.vscode-auto-open-markdown-preview
+
+# pairing
+code --install-extension ms-vsliveshare.vsliveshare
+
+# productivity
+code --install-extension weakish.complete-statement
---
+++ vscode/install-node-extensions.sh
@@ -0,0 +1,32 @@
+# documentation
+code --install-extension stevencl.addDocComments
+
+# linting
+code --install-extension dbaeumer.vscode-eslint
+code --install-extension EditorConfig.EditorConfig
+
+# npm
+code --install-extension christian-kohler.npm-intellisense
+code --install-extension eg2.vscode-npm-script
+code --install-extension fknop.vscode-npm
+
+# serverless
+code --install-extension frankschmid.serverless-vscode
+code --install-extension oderwat.indent-rainbow
+code --install-extension rafwilinski.serverless-vscode-snippets
+
+# testing
+code --install-extension andys8.jest-snippets
+code --install-extension cspotcode.vscode-mocha-latte
+code --install-extension cstechnologies.vscode-jest-runner
+code --install-extension cstechnologies.vscode-mocha-test-runner
+code --install-extension firsttris.vscode-jest-runner
+code --install-extension maty.vscode-mocha-sidebar
+code --install-extension Orta.vscode-jest
+code --install-extension spoonscen.es6-mocha-snippets
+
+# typescript
+code --install-extension eg2.tslint
+code --install-extension pitermarx.auto-format-imports
+code --install-extension steoates.autoimport
+code --install-extension zuoez02.tslint-snippets
